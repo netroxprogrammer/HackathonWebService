23f3a2c7fc66d4006954e93ed3d75d8349bcb810
netroxprogrammer <01112001065026@skt.umt.edu.pk>
2015-11-12 20:38:38 +0500

    Add  Sms

--- Config/Config.php
+++ Config/Config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_USER','umt');
 define('DB_PASSWORD','umt');
-define('DB_NAME','hackathon');
+define('DB_NAME','unity');
 define('DB_HOST','localhost');
 
 ?>
--- GetSdcardContent.php
+++ GetSdcardContent.php
@@ -1,9 +1,13 @@
 <?php
 $response=array();
-if(isset($_GET['submit'])){
+if($_GET['submit']){
 	
 	$response['success']="1";
 	$response['sdcard']="sdcard";
 	echo json_encode($response);
 }
+
 ?>
+<form action="GetSdcardContent.php" method="get">
+<input type="submit" name="submit" />
+</form>
